57ffed56e6fcd18e5d0334566a16d325054da264
casson-projects-2020 <69428882+casson-projects-2020@users.noreply.github.com>
2025-12-17 17:06:42 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,3 +32,12 @@ resource "google_cloudfunctions2_function" "function" {
     available_memory   = "256Mi"
   }
 }
+
+resource "google_cloud_run_service_iam_member" "public_access" {
+  location = google_cloudfunctions2_function.function.location
+  project  = google_cloudfunctions2_function.function.project
+  service  = google_cloudfunctions2_function.function.name 
+
+  role   = "roles/run.invoker"
+  member = "allUsers"
+}

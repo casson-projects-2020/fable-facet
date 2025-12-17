dde362faff61f2ccd3a35f6cb60d1cff47fcab3f
casson-projects-2020 <69428882+casson-projects-2020@users.noreply.github.com>
2025-12-17 15:57:46 -0300

    Add entrypoint script for Terraform setup

---
+++ entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+PROJECT_ID=$(gcloud config get-value project 2>/dev/null)
+REGION="us-central1"
+BUCKET_NAME="${PROJECT_ID}-tfstate-fable"
+
+echo "🚀 Iniciando instalação no projeto: $PROJECT_ID"
+
+if gsutil ls -b "gs://${BUCKET_NAME}" >/dev/null 2>&1; then
+    echo "✅ Bucket already exists."
+else
+    echo "📦 Criating bucket..."
+    gsutil mb -l ${REGION} gs://${BUCKET_NAME}
+    gsutil versioning set on gs://${BUCKET_NAME}
+fi
+
+echo "🛠️ Initing Terraform..."
+terraform init -backend-config="bucket=${BUCKET_NAME}"
+
+terraform apply -auto-approve -var="project_id=${PROJECT_ID}" -var="region=${REGION}"

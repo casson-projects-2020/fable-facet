c79d24455fad61612c77e18bd8e6bfd619fd41e1
casson-projects-2020 <69428882+casson-projects-2020@users.noreply.github.com>
2025-12-19 18:03:47 -0300

    Update entrypoint_new.sh

--- entrypoint_new.sh
+++ entrypoint_new.sh
@@ -6,7 +6,7 @@ pip install flask --user -q
 fuser -k 8080/tcp > /dev/null 2>&1
 
 export FLASK_ENV=production
-python3 server.py > .flask_logs 2>&1 &
+python3 tutorial.py > .flask_logs 2>&1 &
 
 sleep 2
 

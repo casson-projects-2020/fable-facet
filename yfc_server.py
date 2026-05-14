5c0b967bc437b25906c139f8e528eeaffb042aa5
casson-projects-2020 <69428882+casson-projects-2020@users.noreply.github.com>
2026-05-14 14:35:28 -0300

    Refactor print statements and enhance error handling
    Refactor print statements to use flush for immediate output. Introduce fatal_error handling and modify installer route to improve error reporting and user feedback.

--- yfc_server.py
+++ yfc_server.py
@@ -5,9 +5,8 @@ import sys
 import uuid
 import os
 import logging
+import shutil
 
-from google import genai
-from google.api_core import exceptions
 
 os.environ[ 'WERKEZUG_RUN_MAIN' ] = 'true' 
 log = logging.getLogger('werkzeug')
@@ -69,7 +68,7 @@ def create_project():
         invalid = False
 
         project_id = project_name[ : 20 ] + "-" + uuid.uuid4().hex[ :9 ]
-        print( project_name + " " + project_id )
+        print( project_name + " " + project_id, flush = True )
 
         check = subprocess.run(
                 [ 'gcloud', 'projects', 'describe', project_id ],
@@ -77,7 +76,7 @@ def create_project():
         )
 
         if check.returncode == 0: # error, project already exists
-            print( "project already exists" )
+            print( "project already exists", flush = True )
             if attempts > 10:
                 return jsonify({
                     'success': False, 
@@ -87,7 +86,7 @@ def create_project():
                 attempts += 1
             invalid = True
         else:
-            print( "project id is unique and can be used" )
+            print( "project id is unique and can be used", flush = True )
 
     try:
         subprocess.run(
@@ -116,11 +115,73 @@ def create_project():
         }), 500
 
 
+fatal_error = False
+
+
 @app.route( '/get_email', methods=[ 'GET' ])
 def get_email():
     # check if the account is not tied to an organization
+    global fatal_error
 
     project = ""
+
+    try:
+        result = subprocess.run(
+            ["gcloud", "billing", "accounts", "list", "--format=json"],
+            capture_output = True, text = True, check = True
+        )
+        accounts = json.loads( result.stdout )
+
+        # Do not remove this restriction. If no billing account is detected, 
+        # it means you haven't linked one or your account is not personal. 
+        # Several APIs used in YFC require an active Billing Account even 
+        # when using the $300 free credits or the Always Free tier. 
+        # Without it, Google will block requests, but Fable Facet might not 
+        # be able to display the specific error message. Furthermore, if 
+        # your account is not personal and you lack permissions to run this 
+        # command, Fable Facet cannot guarantee the privacy of your secrets 
+        # (such as Gemini API Keys). This could lead to unauthorized quota 
+        # usage, for which Fable Facet is not responsible. 
+        # Please use a personal account as instructed on the website.
+        if not accounts:
+            print( "No billing account detected", flush = True )
+            err_ = "&#x274c;&#xFE0F; Fatal Error: No billing account detected. Your-Fable-Cloud "
+            err_ += "requires one to function. <br/>Please follow the instructions to set up a "
+            err_ += "Billing Account on Fable Facet website."
+            fatal_error = True
+            return err_
+
+    except Exception as e:
+        print( f"Cannot run gcloud billing accounts list: {e.stderr}", flush = True )
+        return "## error, please click restart"
+            
+        # Currencies that indicate that Gemini Free Tier is not provided
+        restricted_currencies = [
+            "EUR", # Europe
+            "GBP", # United Kingdom
+            "CHF", # Switzerland and Liechtenstein
+            "PLN", # Poland
+            "SEK", # Sweden
+            "NOK", # Norway
+            "DKK", # Denmark
+            "CZK", # Czech Republic
+            "HUF", # Hungary
+            "BGN", # Bulgaria
+            "RON", # Romania
+            "ISK"  # Iceland
+        ]    
+    
+        # Do not remove this restriction. If you live in an area where 
+        # Gemini Free Tier is unavailable, using this system will result in 
+        # charges to your GCP billing account, for which Fable Facet 
+        # is not responsible.
+        active_acc = next(( a for a in accounts if a.get( "open" )), None )
+        if active_acc and active_acc.get( "currencyCode" ) in restricted_currencies:
+            err_ = f"&#x274c;&#xFE0F; Fatal Error: currency [{active_acc.get('currencyCode')}] "
+            err_ += "indicates a region (EEA/UK/CH) where Gemini Free Tier is "
+            err_ += "unavailable. Your-Fable-Cloud will not work."
+            fatal_error = True
+            return 
 
     try:
         result = subprocess.run(
@@ -133,7 +194,7 @@ def get_email():
 
     except subprocess.CalledProcessError as e:
         # Caso o gcloud retorne erro (ex: sem conta configurada)
-        print( f"Cannot run gcloud config get-value project: {e.stderr}")
+        print( f"Cannot run gcloud config get-value project: {e.stderr}", flush = True )
         return "## error, please click restart"
 
     try:
@@ -146,7 +207,7 @@ def get_email():
         ancestors = result.stdout
 
     except subprocess.CalledProcessError as e:
-        print( f"Cannot run gcloud projects get-ancestors {project}: {e.stderr}")
+        print( f"Cannot run gcloud projects get-ancestors {project}: {e.stderr}", flush = True )
         return "## error, please click restart"
 
     # This license restriction is a security measure to ensure that secrets 
@@ -157,9 +218,10 @@ def get_email():
     # Bypassing this restriction makes the user solely responsible for any 
     # consequences, as per the Fable Facet Terms of Service on our website.
     if "organization" in ancestors:
-        print( f"account is not personal {ancestors}")
-        err_ = "## This GCP account is part of an organization. Fable Facet's"
-        err_ += "license only allows personal accounts"
+        print( f"account is not personal {ancestors}", flush = True )
+        err_ = "&#x274c;&#xFE0F; Fatal Error: This GCP account is part of an "
+        err_ += "organization. Fable Facet's license only allows personal accounts."
+        fatal_error = True
 
         return err_
     
@@ -174,50 +236,103 @@ def get_email():
 
     except subprocess.CalledProcessError as e:
         # Caso o gcloud retorne erro (ex: sem conta configurada)
-        print(f"Cannot run gcloud config get-value account: {e.stderr}")
+        print( f"Cannot run gcloud config get-value account: {e.stderr}", flush = True )
         return "## error, please click restart"
 
 
 env = os.environ.copy()
 
-@app.route('/validate_key', methods=['POST'])
-def validate_key():
-    data = request.json
-    key = data.get( 'key' )
 
-    if not key:
-        return "no key", 400
-
-    try:
-        client = genai.Client( api_key = key )
-        models = client.models.list()
-
-        env[ "GEMINI_KEY" ] = key
-
-        return "ok", 200
-
-    except Exception as e:
-        return "Invalid key", 401
-
-
-@app.route( '/run_installer', methods=['POST'])
+@app.route( '/run_installer', methods=['GET'])
 def yfc_installer_run():
-    print( "running installer..." )
+    global fatal_error
+
+    def generate( fatal_error ):
+        outp = ""
+
+        if not fatal_error:
+            yield "data: running installer...\n\n";
+
+            process = subprocess.Popen(
+                ['bash', 'yfc_install.sh'],
+                env = env,
+                stdout = subprocess.PIPE,
+                stderr = subprocess.STDOUT, 
+                text = True,
+                bufsize = 1
+            )
+
+            try:
+                for line in iter( process.stdout.readline, '' ):
+                    if outp != "":
+                        yield f"data: {outp}\n\ndata: {line}\n\n"
+                        outp = ""
+                    else:
+                        yield f"data: {line}\n\n"
+                        
+                process.wait()
+
+            finally:
+                # check for errors
+                if process.returncode != 0:
+                    outp = "installer failed, starting rollback...";
+                else:
+                    outp = ""
+                process.terminate() 
+                process.wait()
+
+        else:
+            outp = "installer failed, starting rollback...";
+        
+        # erase the folder fable-facet inside cloudshell_open to avoid
+        # conflicts if the user ever try to install again
+        cwd = os.getcwd()
+        home_dir = os.path.expanduser( "~" )
+        safe_ = os.path.join( home_dir, "cloudshell_open" )
+        is_target = os.path.basename( cwd ) == "fable-facet"
+    
+        cmd = f"""
+(
+sleep 2
+"""
+        if os.path.commonpath([ cwd, safe_ ]) == safe_ and cwd != safe_ and is_target:
+            cmd += f"rm -rf '{cwd}'\n"
+
+            if outp != "":
+                cmd += "Fatal Error: "
+            
+            cmd += f"Removing repo folder: {cwd}\nclear\n"
+
+        if outp != "":
+            cmd += """) | dialog --cr-wrap --programbox "✅ Success: \ 
+Your-Fable-Cloud is installed. Get back to Fable Facet site to use it\n\n\
+this script created one bucket on Cloud Storage, and one Cloud Run Function.\n\
+If you want to uninstall it, see instructions on Fable Facet site site:\n\
+in Tech section, 'How to delete my account'\n\n\
+You can now close the browser tab and Cloud Shell and return to Fable Facet \
+site" 30 80 > /dev/tty
+""" 
+        else:
+            cmd += """) | dialog --cr-wrap --programbox "❌ Fatal Error: \
+cannot install Your-Fable-Cloud.\n\n\
+If the error appears to be temporaty you may try to install again.\n\
+Close the browser tab and Cloud Shell and return to Fable Facet site\n\n\
+Please contact us." 30 80 > /dev/tty
+""" 
+        subprocess.Popen( cmd, shell = True )
+
+        # send os._exit(0) after a small delay to allow Flask to send 
+        # the last package in the connection
+        import _thread
+        def kill_later():
+            import time
+            time.sleep( 1 )
+            os._exit( 0 )
+
+        _thread.start_new_thread( kill_later, ())
 
 
-    subprocess.Popen(
-        ['bash', 'yfc_install.sh'],
-        env = env,
-        stdout = None, 
-        stderr = None,
-        bufsize = 1
-    )
-
-    return jsonify(
-    {
-        'success': True, 
-        'message': f"Installer running"
-    }), 200
+    return Response( generate( fatal_error ), mimetype='text/event-stream' )
 
 
 if __name__ == '__main__':

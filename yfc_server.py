60b83e8f387b069c287bbe776ada0a4bad3e0abb
casson-projects-2020 <69428882+casson-projects-2020@users.noreply.github.com>
2026-05-15 11:05:31 -0300

    Implement billing account checks in project setup
    Add billing account verification and linking for projects.

--- yfc_server.py
+++ yfc_server.py
@@ -29,9 +29,58 @@ def index():
     return render_template( 'index.html', projects = projects )
 
 
+fatal_error = False
+
+
+def billing_problems( project_id ):
+    global fatal_error
+ 
+    # verifica se o projeto ja tem uma billing account
+    result = subprocess.run(
+        ['gcloud', 'billing ', 'projects', 'describe', project_id, '--format=json' ],
+        stdout=sys.stdout,
+        stderr=sys.stderr
+    )
+    billing = json.loads( result.stdout.strip())
+    
+    if( billing[ "billingEnabled" ] != True ):
+        # obtem a conta q foi criada antes de rodar este script e associa ao projeto
+        result = subprocess.run(
+            ["gcloud", "billing", "accounts", "list", "--format=json"],
+            capture_output = True, text = True, check = True
+        )
+        accounts = json.loads( result.stdout )
+
+        if not accounts:
+            print( "No billing account detected", flush = True )
+            err_ = "&#x274c;&#xFE0F; Fatal Error: No billing account detected. Your-Fable-Cloud "
+            err_ += "requires one to function. <br/>Please follow the instructions to set up a "
+            err_ += "Billing Account on Fable Facet website."
+            fatal_error = True
+            
+            return jsonify(
+            {
+                'success': False, 
+                'error': err_
+            }), 500
+
+        else:
+            active_acc = next(( a for a in accounts if a.get( "open" )), None )
+            acc_name = active_acc[ "name" ]
+            acc_id = acc_name[ acc_name.find( "/" ) + 1 : ]
+        
+            subprocess.run(
+                [   'gcloud', 'billing', 'projects', 'link', 
+                    project_id, '--billing-account', acc_id ],
+                stdout=sys.stdout,
+                stderr=sys.stderr
+            )
+            
+            return None
+
+
 @app.route( '/set-project', methods=['POST'])
 def set_project():
-
     data = request.json
     project_name_id = data.get( 'name_id' ).split( "(" )
     project_id = project_name_id[ 1 ].replace( ")", "" ).strip()
@@ -43,11 +92,14 @@ def set_project():
             stderr=sys.stderr
         )
 
-        return jsonify(
-        {
-            'success': True, 
-            'message': f"Project '{project_id}' selected"
-        }), 200
+        if ( res := billing_problems( project_id )) is None:
+            return jsonify(
+            {
+                'success': True, 
+                'message': f"Project '{project_id}' selected"
+            }), 200
+        else:
+            return res
 
     except Exception as e:
         return jsonify(
@@ -102,20 +154,20 @@ def create_project():
             stderr=sys.stderr 
         )
 
-        return jsonify(
-        {
-            'success': True, 
-            'message': f"Project '{project_name}' created and selected"
-        }), 200
+        if ( res := billing_problems( project_id )) is None:
+            return jsonify(
+            {
+                'success': True, 
+                'message': f"Project '{project_id}' created and selected"
+            }), 200
+        else:
+            return res
 
     except Exception as e:
         return jsonify({
             'success': False, 
             'error': f"gcloud failed to create the project {e}"
         }), 500
-
-
-fatal_error = False
 
 
 @app.route( '/get_email', methods=[ 'GET' ])
@@ -223,12 +275,12 @@ def yfc_installer_run():
             )
 
             try:
-                for line in iter( process.stdout.readline, '' ):
-                    if outp != "":
-                        yield f"data: {outp}\n\ndata: {line}\n\n"
-                        outp = ""
-                    else:
-                        yield f"data: {line}\n\n"
+                while True:
+                  line = process.stdout.readline()
+                  if not line and process.poll() is not None:
+                      break
+                  if line:
+                      yield f"data: {line.strip()}\n\n"
                         
                 process.wait()
 
@@ -243,10 +295,10 @@ def yfc_installer_run():
 
         else:
             outp = "installer failed, starting rollback...";
-
+        
         # Envia uma mensagem de finalização para o JS saber que acabou antes do crash
         yield f"data: internal_status:finished_{'ok' if not outp else 'error'}\n\n"
-
+        
         # erase the folder fable-facet inside cloudshell_open to avoid
         # conflicts if the user ever try to install again
         cwd = os.getcwd()
@@ -267,7 +319,7 @@ sleep 2
             cmd += f"Removing repo folder: {cwd}\nclear\n"
 
         if outp != "":
-            cmd += """) | dialog --cr-wrap --programbox "✅ Success: \\ 
+            cmd += """) | dialog --cr-wrap --programbox "Success: \\ 
 Your-Fable-Cloud is installed. Get back to Fable Facet site to use it\n\n\\
 this script created one bucket on Cloud Storage, and one Cloud Run Function.\n\\
 If you want to uninstall it, see instructions on Fable Facet site site:\n\\
@@ -276,7 +328,7 @@ You can now close the browser tab and Cloud Shell and return to Fable Facet \\
 site" 30 80 > /dev/tty
 """ 
         else:
-            cmd += """) | dialog --cr-wrap --programbox "❌ Fatal Error: \\
+            cmd += """) | dialog --cr-wrap --programbox "Fatal Error: \\
 cannot install Your-Fable-Cloud.\n\n\\
 If the error appears to be temporaty you may try to install again.\n\\
 Close the browser tab and Cloud Shell and return to Fable Facet site\n\n\\
@@ -289,7 +341,7 @@ Please contact us." 30 80 > /dev/tty
         import _thread
         def kill_later():
             import time
-            time.sleep( 1 )
+            time.sleep( 2 )
             os._exit( 0 )
 
         _thread.start_new_thread( kill_later, ())

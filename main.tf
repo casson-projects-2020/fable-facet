6029d794faf9004925c1c7147629f28a2fe9f455
casson-projects-2020 <69428882+casson-projects-2020@users.noreply.github.com>
2026-05-19 00:09:39 -0300

    Add IAM members for OpenID and token creation

--- main.tf
+++ main.tf
@@ -89,10 +89,22 @@ resource "google_service_account_iam_member" "allow_openid_impersonation" {
   member             = "user:${local.email}"
 }
 
+resource "google_service_account_iam_member" "allow_openid_impersonation" {
+  service_account_id = google_service_account.function_sa.name
+  role               = "roles/iam.serviceAccountOpenIdTokenCreator"
+  member             = "serviceAccount:${google_service_account.function_sa.email}"
+}
+
 resource "google_service_account_iam_member" "allow_token_creation" {
   service_account_id = google_service_account.function_sa.name
   role               = "roles/iam.serviceAccountTokenCreator"
   member             = "user:${local.email}" 
+}
+
+resource "google_service_account_iam_member" "allow_token_creation" {
+  service_account_id = google_service_account.function_sa.name
+  role               = "roles/iam.serviceAccountTokenCreator"
+  member             = "serviceAccount:${google_service_account.function_sa.email}" 
 }
 
 resource "google_storage_bucket_iam_member" "function_storage_access" {
